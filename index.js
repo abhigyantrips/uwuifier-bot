725adb1eb5261b7713c01c20fb8b6b222ebda68e
Abhigyan Tripathi <abhigyantrips@gmail.com>
2022-01-31 10:26:40 +0530

    feat: Add timestamp to reply embeds.

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ client.on("messageCreate", async (message) => {
 				username: message.author.username,
 				avatarURL: message.author.avatarURL(),
 				disableMentions: "everyone",
+				files: message.attachments.map(file => file.url)
 			});
 			await message.delete();
 			return;
@@ -54,11 +55,12 @@ client.on("messageCreate", async (message) => {
 	
 	for (let server of servers) {
 		if (server.serverChannel === message.channel.id) {
-			const reference = await message.fetchReference()
+			const reference = await message.fetchReference();
 			embed = new Discord.MessageEmbed()
 				.setColor("#303136")
 				.setAuthor({ name: reference.author.username, iconURL: reference.author.avatarURL(), url: reference.url })
 				.setDescription(reference.content)
+				.setTimestamp(reference.createdTimestamp);
 			await server.serverWebhook.send({
 				content: uwuifier.uwuifySentence(message.content),
 				username: message.author.username,
